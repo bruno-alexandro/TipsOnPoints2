503a1ce0d59f5d4ddffef64116a2aeb564771894
bruno-alexandro <bruno.alexandro333@gmail.com>
2024-04-17 22:22:37 -0300

    melhorado prompt para novas cartas

--- Services/CardsService.cs
+++ Services/CardsService.cs
@@ -4,14 +4,23 @@ namespace TipsOnPoints.Services
     public class CardsService : ICardsService
     {
         public readonly IOpenAiService _openAiService;
+
+        public readonly String _testTheme = "Lugares";
+        public readonly String _testSingleTheme = "Lugar";
+        public readonly int _testQtyTips = 50;
+        public readonly String _testThemeComplements = "Locais históricos, Cidades, Paises, Estados, Monumentos, Parques";
+        public readonly String _testCompleteText = "";
         public CardsService(IOpenAiService openApiService){
             _openAiService = openApiService;
+            _testCompleteText = $"Você é um oraculo que sabe tudo sobre {_testTheme} que podem ser {_testThemeComplements} "+
+                                $"que são famosos no mundo. Sorteie um {_testSingleTheme} qualquer "+
+                                $"e de {_testQtyTips} dicas para que uma pessoa consiga adivinhar muito facilmente baseado nas suas dicas sem que você mensione ele. "+
+                                $"Me retorne um JSON com as dicas e a resposta, somente o JSON e nada mais";
         }
         public async Task<string> GetNewCard()
         {
-            //var result = _openAiService.
-            var text = "você é um oraculo que sabe tudo sobre lugares famosos no mundo, você consegue escolher um lugar e dar 10 dicas para que uma pessoa com conhecimento baixo sobre esse lugar consiga acertar que lugar é baseado nas suas dicas sem que você mensione ele. Baseado nesse contexto escolha um lugar, liste de 1 a 10 e no final escreva: Resposta: (aqui você coloca a resposta). Coloque somente a lista e a resposta, nada mais";
-            var result = await _openAiService.GetNewCard(text);
+
+            var result = await _openAiService.GetNewCard(_testCompleteText);
             
             return result;
         }
