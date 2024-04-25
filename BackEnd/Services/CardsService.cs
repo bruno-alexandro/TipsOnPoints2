16dd7b3b3e9fbe9a3a9f1a937907f964938736ec
Bruno Alexandro Vieira <bruno.vieira@solonetwork.com.br>
2024-04-25 18:29:07 -0300

    Criado logica para chamar carta com tema aleatorio salvo no BD

--- BackEnd/Services/CardsService.cs
+++ BackEnd/Services/CardsService.cs
@@ -1,6 +1,4 @@
 
-using System.Numerics;
-using System.Text.Json;
 using TipsOnPoints.Models;
 using TipsOnPoints.Repositories;
 
@@ -11,34 +9,37 @@ namespace TipsOnPoints.Services
         public readonly IOpenAiService _openAiService;
         public readonly ICardRepository _cardRepository;
         public readonly ICardTipRepository _cardTipRepository;
+        public readonly IThemeService   _themeService;
 
         // public readonly int _testThemeId = 1;
         // public readonly String _testTheme = "Lugares";
         // public readonly String _testSingleTheme = "Lugar";
         // public readonly int _testQtyTips = 50;
         // public readonly String _testThemeComplements = "Locais históricos, Cidades, Paises, Estados, Monumentos, Parques";
-        public Theme _theme;
+        //public Theme _theme;
 
-        public CardsService(IOpenAiService openApiService, ICardRepository cardRepository, ICardTipRepository cardTipRepository){
+        public CardsService(IOpenAiService openApiService, ICardRepository cardRepository, ICardTipRepository cardTipRepository, IThemeService themeService){
             _openAiService = openApiService;
             _cardRepository = cardRepository;
             _cardTipRepository = cardTipRepository;
-            _theme = new Theme();
+            //_theme = new Theme();
+            _themeService = themeService;
         }
 
         public string GetFullPromptText(Theme theme)
         {
-            string fullPrompText = $"Você é um oraculo que sabe tudo sobre {theme.PluralTheme} que podem ser {theme.ThemeComplements} "+
+            string themeComplements = theme.ThemeComplements?.Count == 0 || theme.ThemeComplements == null ? " quaisquer " : string.Join(", ", theme.ThemeComplements);
+
+            string fullPrompText = $"Você é um oraculo que sabe tudo sobre {theme.PluralTheme} que podem ser {themeComplements} "+
                                    $"que são famosos no mundo. Sorteie um {theme.SingleTheme} qualquer "+
                                    $"e de {theme.QtyTips} dicas para que uma pessoa consiga adivinhar muito facilmente baseado nas suas dicas sem que você mensione ele. "+
                                    $"Me retorne um JSON com SEMPRE com as dicas e a resposta, somente o JSON e nada mais, no JSON as dicas virão como dicas: [], resposta: ";
             
             return fullPrompText;
         }
-
-        public async Task<string> GetNewCard(Theme theme)
+        public async Task<string> GetNewCard()
         {
-            var fullPrompText = GetFullPromptText(theme);
+            var fullPrompText = GetFullPromptText(_themeService.GetSortedTheme());
             var result = await _openAiService.GetNewCard(fullPrompText);
             return result; 
         }
@@ -56,7 +57,7 @@ namespace TipsOnPoints.Services
                 var cardTip = new CardTips
                 {
                     Tip = tip,
-                    CardId = card.Id // Agora, o ID do card está preenchido após chamar SaveChanges()
+                    CardId = card.Id
                 };
 
                 _cardTipRepository.Add(cardTip);
